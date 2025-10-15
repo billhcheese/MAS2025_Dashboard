6b25bbcc1d0d1d6851aa6dd8fa2b060b6e50c8a0
Bill Huang <billhcheese@gmail.com>
2025-10-15 15:02:53 -0400

    expanded test2

--- Metro_Summary.py
+++ Metro_Summary.py
@@ -5,7 +5,7 @@ import textwrap
 import streamlit.components.v1 as components
 
 st.set_page_config(page_icon="Assets/metro-atl-speaks.svg")
-st.set_page_config(initial_sidebar_state="expanded")
+st.set_page_config(initial_sidebar_state="collapsed")
 
 # Cache data loading for better performance
 @st.cache_data
